e3ad6a6bf90de38e23773f1cf43d4a23a222376e
Gxmestk <gt.khemniwat@gmail.com>
2022-10-14 19:37:28 +0700

    prepare for fuck up

--- srcs/sorting/easy_sort.c
+++ srcs/sorting/easy_sort.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 10:16:11 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/10/13 14:36:04 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/10/14 19:36:50 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,35 +44,49 @@ static t_node	*max_val_node(t_dlst *stack)
 	return (max);
 }
 
-void	easy_sort(t_dlst *stack, int s)
+void	easy_sort(t_dlst *stack_a, t_dlst *stack_b, int s)
 {
 	t_node	*curr;
 	t_node	*min;
 	t_node	*max;
 	t_node	*ncurr;
 	int		i;
+	int		j;
 
 	i = 0;
-	min = min_val_node(stack);
-	max = max_val_node(stack);
+	j = 0;
+	min = min_val_node(stack_a);
+	max = max_val_node(stack_a);
 	if (s == A)
 	{
-		while (!dlst_issort(stack, stack->first, ASC))
+		while (!dlst_issort(stack_a, stack->first, ASC))
 		{
+			min = min_val_node(stack_a);
 			ft_printf("i = %d   ", i);
-			curr = stack->last;
-			ncurr = stack->first;
+			curr = stack_a->last;
+			ncurr = stack_a->first;
+			if (curr == min_val_node(stack_a))
+			{
+				push(stack_a, stack_b);
+				j++;
+			}
 			if (((curr->val > curr->prev->val) && !(curr->prev == min && curr == max) && ncurr))
 				swap(stack, s);
-			else if (curr->val > stack->first->val)
+			else if (curr->val > stack_a->first->val)
 				if (curr == min)
-					swap(stack, s);
+					swap(stack_a, s);
 				else
-					rotate(stack, s);
+					rotate(stack_a, s);
 			else
-				rrotate(stack, s);
+				rrotate(stack_a, s);
 			if (i == 50)
 				break ;
+			i++;
+		}
+		i = 0;
+		while (i < j)
+		{
+			push(stack_b, stack_a);
 			i++;
 		}
 	}
